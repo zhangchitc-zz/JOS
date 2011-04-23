4d48b12ab70eb1886e55eec2cd7e63f0742f8b57
Vorstro1400 <zhangchitc@gmail.com>
2011-04-24 02:03:05 +0800

    [Checked] Finished PartB without COW

--- kern/syscall.c
+++ kern/syscall.c
@@ -147,7 +147,14 @@ static int
 sys_env_set_pgfault_upcall(envid_t envid, void *func)
 {
 	// LAB 4: Your code here.
-	panic("sys_env_set_pgfault_upcall not implemented");
+
+    struct Env *e;
+    if (envid2env (envid, &e, 1) < 0)
+        return -E_BAD_ENV;
+
+    e->env_pgfault_upcall = func;
+
+    return 0;
 }
 
 // Allocate a page of memory and map it at 'va' with permission
@@ -443,6 +450,10 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 
         case SYS_page_unmap:
             r = sys_page_unmap ((envid_t) a1, (void*) a2);
+            break;
+
+        case SYS_env_set_pgfault_upcall:
+            r = sys_env_set_pgfault_upcall ((envid_t) a1, (void*) a2);
             break;
 
         default:
